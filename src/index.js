c12b668003b3f5d0dad248524e2b85cea8290b14
Pedro H Birais <phbirais@gmail.com>
2021-07-12 01:24:08 -0300

    Finish all routes

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ function checksExistsUserAccount(request, response, next) {
 
   //se n existe o user
   if (!user) {
-    return response.status(400).json({ error: "User not found" });
+    return response.status(404).json({ error: "User not found" });
   }
 
   request.user = user;//repassar para as rotas
@@ -34,6 +34,23 @@ function checksExistsUserAccount(request, response, next) {
   //se existir pode prosseguir
 
   return next();
+}
+
+function checksExistsTodo(request, response, next) {
+
+  const { id } = request.params;
+  const { user } = request; //recebendo user pelo middleware
+
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+
+  if (todoIndex < 0) {
+    return response.status(404).json({ error: "todo not found" });
+  }
+
+  request.todoIndex = todoIndex;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
@@ -86,20 +103,60 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 
   user.todos.push(todoOperation);
 
-  return response.status(201).json(todos[todos.length - 1]);
+  return response.status(201).json(user.todos[user.todos.length - 1]);
 
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+//atualizar o todo criado
+app.put('/todos/:id', checksExistsUserAccount, checksExistsTodo, (request, response) => {
   // Complete aqui
+  const { title, deadline } = request.body;
+  const { id } = request.params; //recebendo id pela rota
+  const { user, todoIndex } = request; //recebendo user e index pelo middleware
+
+  /*const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+
+  //console.log(index);
+
+  //se n existe o todo
+  if (todoIndex < 0) {
+    return response.status(404).json({ error: "todo not found" });
+  }*/
+
+  //modificar todo de index encontrado
+  user.todos[todoIndex] = {
+    id: id,
+    title: title,
+    done: user.todos[todoIndex].done,
+    deadline: new Date(deadline),
+    created_at: user.todos[todoIndex].created_at,
+  };
+
+  return response.status(201).json(user.todos[todoIndex]);
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+//alterar Todo para done
+app.patch('/todos/:id/done', checksExistsUserAccount, checksExistsTodo, (request, response) => {
+
+  const { user, todoIndex } = request;
+
+  //alterando todo para done
+  user.todos[todoIndex].done = true;
+
+  //retornando a requisição com o objeto todo
+  return response.status(200).json(user.todos[todoIndex]);
+
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+//delete Todo
+app.delete('/todos/:id', checksExistsUserAccount, checksExistsTodo, (request, response) => {
+  const { user, todoIndex } = request;
+
+  //deletar todo escolhido
+  user.todos.splice(todoIndex, 1);
+
+  //se deletar funcionar
+  return response.status(204).send();
 });
 
 module.exports = app;
